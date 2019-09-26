345b3d990120f5690303080280eb6916a31352e0
RifianJoe <55788355+RifianJoe@users.noreply.github.com>
2019-09-26 15:15:58 +0700

    Add files via upload

---
+++ IF1-10118013-Latihan15-Logika/src/if1/pkg10118013/latihan15/logika/IF110118013Latihan15Logika.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10118013.latihan15.logika;
+
+/**
+ *
+ * @author Rifian
+ * Nama : Rifian Joe Premilenio
+ * Kelas : IF-1
+ * NIM : 10118013
+ * Deskripsi Program : Program ini berisi program untuk menampilkan jenis-jenis tipe data bilangan bulat
+ */
+public class IF110118013Latihan15Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        boolean a = true;
+        boolean b = false;
+        System.out.println("a && b = " + (a && b));
+        System.out.println("a || b = " + (a || b));
+        System.out.println("!a = " + !a);
+        System.out.println("!(a && b) = " + !(a && b));
+    }
+    
+}
